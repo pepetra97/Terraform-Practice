bb5ccf6b2df17d5abc8d40b69cfda5c7c8c8cba9
pepetra97 <pepetra97@gmail.com>
2023-04-27 15:52:37 +0200

    Creating sc-group and attach it to ec2 instance

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -3,11 +3,40 @@ provider "aws" {
   shared_credentials_files = [ "C:\\Users\\pepet\\.aws\\credentials" ]
 }
 
+# EC2 instance
+
 resource "aws_instance" "my-first-ec2" {
   ami           = "ami-0a695f0d95cefc163"
   instance_type = "t2.micro"
+  security_groups = [ "${aws_security_group.sec-group.name}" ]
 
   tags = {
     Name = "Terraform-test-ec2"
+  }
+}
+
+#security group
+
+resource "aws_security_group" "sec-group" {
+  name        = "sec-group"
+
+  ingress {
+    # Block all incoming traffic
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sec-group"
   }
 }
